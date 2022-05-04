9e2a8bf69b6e935bb10cc0d2e1435d224d09c26f
Joshua Mankelow <joshua@mankelow.org>
2022-05-04 17:29:02 +0100

    fixed some more stuff

--- main.py
+++ main.py
@@ -96,12 +96,60 @@ class Bo3Finals(FixtureFormat):
                 return [self.teams[1], self.teams[0]]
 
 
+class BoXFinals(FixtureFormat):
+    def __init__(self, teams: list[Team], X: int):
+        super().__init__(teams)
+        
+        if X % 2 != 1:
+            raise TournamentException(f"BoX format not valid for X = {X}")
+
+        self.X = X
+        
+        self.games = [Map(teams) for _ in range(X)]
+
+    def run(self):
+        team1_score = 0
+        team2_score = 0
+
+        for game in self.games:
+            res = game.run()
+            if res[0] == self.teams[0]:
+                team1_score += 1
+            else:
+                team2_score += 1
+
+            if team1_score == self.X//2 + 1:
+                return [self.teams[0], self.teams[1]]
+
+            if team2_score == self.X//2 + 1:
+                return [self.teams[1], self.teams[0]]
+
+
 class Bo5Finals(FixtureFormat):
     def __init__(self, teams: list[Team]):
         super().__init__(teams)
+        self.BoX = BoXFinals(teams, 5)
 
     def run(self):
-        pass
+        return self.BoX.run()
+
+
+class Bo7Finals(FixtureFormat):
+    def __init__(self, teams: list[Team]):
+        super().__init__(teams)
+        self.BoX = BoXFinals(teams, 7)
+
+    def run(self):
+        return self.BoX.run()
+
+
+class Bo9Finals(FixtureFormat):
+    def __init__(self, teams: list[Team]):
+        super().__init__(teams)
+        self.BoX = BoXFinals(teams, 9)
+
+    def run(self):
+        return self.BoX.run()
 
 
 class Simulator:
@@ -126,21 +174,47 @@ class Simulator:
 
 
 if __name__ == "__main__":
-    warwick_angels = Team("Grey Warwick", 2000)
+    warwick_angels = Team("Warwick Angels", 2000)
     portsmouth_paladins = Team("Portsmouth Paladins", 2200)
-
     teams = [warwick_angels, portsmouth_paladins]
 
-    s = Simulator(Bo1Finals, teams, 0.5)
-    results = s.simulate(10)
-    a_results = aggregate_wins(teams, results)
-    print(results)
-    print(a_results)
+    print(f"{warwick_angels} elo: {warwick_angels.measured_skill}")
+    print(f"{portsmouth_paladins} elo: {portsmouth_paladins.measured_skill}")
+    
+    print("=================================")
 
-    print("================")
+    measurement_error_std = 200
 
-    s2 = Simulator(Bo3Finals, teams, 0.5)
-    results2 = s2.simulate(10)
-    a_results2 = aggregate_wins(teams, results2)
-    print(results2)
-    print(a_results2)
+    bo1 = Simulator(Bo1Finals, teams, measurement_error_std)
+    bo3 = Simulator(Bo3Finals, teams, measurement_error_std)
+    bo5 = Simulator(Bo5Finals, teams, measurement_error_std)
+    bo7 = Simulator(Bo7Finals, teams, measurement_error_std)
+    bo9 = Simulator(Bo9Finals, teams, measurement_error_std)
+
+    bo1_results = bo1.simulate(10000)
+    bo3_results = bo3.simulate(10000)
+    bo5_results = bo5.simulate(10000)
+    bo7_results = bo7.simulate(10000)
+    bo9_results = bo9.simulate(10000)
+
+    bo1_data = aggregate_wins(teams, bo1_results)
+    bo3_data = aggregate_wins(teams, bo3_results)
+    bo5_data = aggregate_wins(teams, bo5_results)
+    bo7_data = aggregate_wins(teams, bo7_results)
+    bo9_data = aggregate_wins(teams, bo9_results)
+
+    print("========== BO1 RESULTS ==========")
+    print(teams)
+    print(bo1_data)
+    print("========== BO3 RESULTS ==========")
+    print(teams)
+    print(bo3_data)
+    print("========== BO5 RESULTS ==========")
+    print(teams)
+    print(bo5_data)
+    print("========== BO7 RESULTS ==========")
+    print(teams)
+    print(bo7_data)
+    print("========== BO9 RESULTS ==========")
+    print(teams)
+    print(bo9_data)
